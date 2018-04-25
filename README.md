314f52f212ee0a111d9a2c5768c40605aaec2545
Lee Junseok <jslee@junseoklee.local>
2018-04-25 14:26:54 +0900

    chat server and client

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # scp_matbogi
 Matbogi project for BOSCoin - prototyping SCP 
+
+
+## Dependencies
+```
+python3
+pip3 install twisted
+```
+
+## Assumptions
+* instead of a p2p model, this implementation is a client-server model where the server broadcasts 
+all the client's messages to the other clients.
+* assume that the server is not corrupt and the connection between the clients are secure
+## Implementation
+
+## TODOs
+
+
+## References
+
+* [P2P Programming Framework - Python] (http://cs.berry.edu/~nhamid/p2p/framework-python.html)
---
+++ src/node.py
@@ -0,0 +1,60 @@
+
+class Node:
+    def __init__(self, nodeID):
+        self.nodeID = nodeID
+        self.threshold = 4
+        self.voted = dict()
+        self.accepted = dict()
+        self.confirmed = dict()
+
+    def __repr__(self):
+        return '<Node: %s>' % self.nodeID
+
+    def set_qset(self, node):
+        self.qset.append(node)
+
+    def set_init(self):
+        self.voted = dict()
+        self.accepted = dict()
+        self.confirmed = dict()
+
+    def vote(self, message):
+        if message not in self.voted:
+            self.voted[message] = 1
+        else:
+            self.voted[message] += 1
+        if self.voted[message] >= self.threshold:
+            self.voted.pop(message)
+            self.accept(message)
+
+    def accept(self, message):
+        if message not in self.accepted:
+            self.accepted[message] = 1
+        else:
+            self.accepted[message] += 1
+        if self.accepted[message] >= self.threshold:
+            self.accepted.pop(message)
+            self.confirm(message)
+
+    def confirm(self, message):
+        if message not in self.confirmed:
+            self.confirmed[message] = 1
+        else:
+            self.confirmed[message] += 1
+        if self.confirmed[message] >= self.threshold:
+            return "message is confirmed"
+
+    def send_message(self, name):
+        print("sent message")
+
+    def broadcast(self):
+        for node in self.qset:
+            self.send_message(node.name)
+
+    def receive_message(self, name):
+        print("receivemessage")
+
+
+
+
+
